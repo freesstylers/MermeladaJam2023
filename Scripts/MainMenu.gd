4f96e010618ef5278906e3657b52f280cb89845a
IgnOry <37537092+IgnOry@users.noreply.github.com>
2023-11-17 23:42:29 +0100

    Temp Main Menu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,43 +1,38 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
-func _on_TextureButton2_button_down():
-	FreeStylers()
-	pass
-	
-
-func _on_TextureButton_button_down():
-	GameJam()
+func NormalMode():
+	get_tree().change_scene("res://Escenas/MainScene.tscn")
 	pass # Replace with function body.
 
+func EndlessMode():
+	pass # Replace with function body.
 
-func _on_TextureButton3_button_down():
-	Twitter()
+func Credits():
+	get_node("CreditsContainer").visible = true
+	get_node("MainMenuContainer").visible = false
+	pass # Replace with function body.
+	
+func CreditsBack():
+	get_node("CreditsContainer").visible = false
+	get_node("MainMenuContainer").visible = true
 	pass # Replace with function body.
 
 func FreeStylers():
 	OS.shell_open("https://freestylers-studio.itch.io/")
 	pass # Replace with function body.
 
-func GameJam():
+func Jam():
 	OS.shell_open("https://itch.io/jam/mermelada-jam")
 	pass # Replace with function body.
-	
+
 func Twitter():
 	OS.shell_open("https://twitter.com/FreeStylers_Dev")
 	pass # Replace with function body.
